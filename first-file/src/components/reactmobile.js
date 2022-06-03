89eb41aaf7b5a6fa87d821693d89441ddc94b221
Swapnil yadav <yadavswapnil712@gmail.com>
2022-06-03 22:21:52 +0530

    2-commit

---
+++ first-file/src/components/reactmobile.js
@@ -0,0 +1,30 @@
+import React from 'react';
+
+
+const Mobile = ()  => {
+
+   return(
+       <>
+       <h1>Mobile Operating System</h1>
+        <ul>
+          <li>Android</li>
+          <li>Blackberry</li>
+          <li>Iphone</li>
+          <li>Windows phone</li>
+       
+        </ul>
+
+        <h1>Mobile Manufacture </h1>
+        <ul>
+          <li>samsung</li>
+          <li>HTC</li>
+          <li>Micromax</li>
+          <li>Apple</li>
+       
+        </ul> 
+      </> 
+   )
+
+}
+
+export default Mobile;
